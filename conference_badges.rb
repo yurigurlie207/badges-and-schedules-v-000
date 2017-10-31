b229b343720451ff710b0e8e6f42a776625c9fc9
Adrienne Yao <adrienne.yao@icloud.com>
2017-10-31 21:26:14 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,32 @@
 # Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(namelist)
+  myArray = Array.new
+  namelist.each do |name|
+    myArray.push("Hello, my name is #{name}.")
+  end
+
+  return myArray
+end
+
+def assign_rooms(namelist)
+  myArray = Array.new
+  namelist.each_with_index do |name, index|
+    myArray.push("Hello, #{name}! You'll be assigned to room #{index + 1}!")
+  end
+
+  return myArray
+end
+
+def printer(namelist)
+
+  batch_badge_creator(namelist).each do |name|
+    puts "#{name}"
+  end
+  assign_rooms(namelist).each do |name|
+    puts "#{name}"
+  end
+end
