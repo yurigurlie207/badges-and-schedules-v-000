eb4f0dd45e09ddcd8890a8887846e9c44e4f3a9c
Adrienne Yao <adrienne.yao@icloud.com>
2017-10-31 21:16:32 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -15,7 +15,7 @@ end
 def assign_rooms(namelist)
   myArray = Array.new
   namelist.each do |name, index|
-    myArray.push("Hello #{name}! You'll be assigned to room #{index + 1}")
+    myArray.push("Hello #{name}! You'll be assigned to room #{index + 1}!")
   end
 
   return myArray
